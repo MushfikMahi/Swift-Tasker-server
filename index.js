c5864cc2d0c71bdf575168fc0602ac5e7fb4e459
MushfikMahi <salehinmahi2004@gmail.com>
2024-06-02 04:24:02 +0600

    send the jwt token to the client side

--- index.js
+++ index.js
@@ -35,6 +35,21 @@ async function run() {
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
 
+    // auth related api
+    app.post("/jwt", async (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: "365d",
+      });
+      res
+        .cookie("token", token, {
+          httpOnly: true,
+          secure: process.env.NODE_ENV === "production",
+          sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+        })
+        .send({ success: true });
+    });
+
     // save a user data in db
     app.put("/user", async (req, res) => {
       const user = req.body;

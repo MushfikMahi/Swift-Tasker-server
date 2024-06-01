3b6b662248e85700383baef75c35df9433cb5b74
MushfikMahi <salehinmahi2004@gmail.com>
2024-06-02 04:33:33 +0600

    remove the token when logged out

--- index.js
+++ index.js
@@ -50,6 +50,22 @@ async function run() {
         .send({ success: true });
     });
 
+    // Logout
+    app.get("/logout", async (req, res) => {
+      try {
+        res
+          .clearCookie("token", {
+            maxAge: 0,
+            secure: process.env.NODE_ENV === "production",
+            sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+          })
+          .send({ success: true });
+        console.log("Logout successful");
+      } catch (err) {
+        res.status(500).send(err);
+      }
+    });
+
     // save a user data in db
     app.put("/user", async (req, res) => {
       const user = req.body;
